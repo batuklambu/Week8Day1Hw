54e4ef66b67655c502acc8a8e4e7eab2fa5b2775
batuklambu <batuklambu@gmail.com>
2023-01-25 09:17:25 -0500

    Week8day1HW

---
+++ Week8Day1.js
@@ -0,0 +1,30 @@
+// // How do you make a Circle in CSS?
+// To create a circle we can set the border-radius on the element. This will create curved corners on the element.
+// If we set it to 50% it will create a circle
+
+// // What is Z-index?
+// Z Index ( z-index ) is a CSS property that defines the order of overlapping HTML elements. Elements with a higher index will be placed on
+// top of elements with a lower index. Note: Z index only works on positioned elements
+// ( position:absolute , position:relative , or position:fixed ).
+
+// // What is a Vendor Prefix and What is it's Purpose?
+
+// CSS Vendor prefixes (or browser prefixes) are a way for browsers to give access to new CSS features not yet considered stable. By using prefixes,
+// we can use these newer CSS features with the browsers that support them — instead of waiting for all browsers to catch up.
+
+// // What is the difference between CSS Grid and Flexbox?
+// Grid and flexbox. The basic difference between CSS Grid Layout and CSS Flexbox Layout is that flexbox was designed for layout in one
+// dimension - either a row or a column. Grid was designed for two-dimensional layout - rows, and columns at the same time.
+
+// // How might you Speed Up a Slow App? hint Google Lighthouse in Chrome Developer Tools....
+// Lighthouse is an automated tool for improving the quality of your site. You give it a URL, and it provides a list of recommendations
+// on how to improve page performance, make pages more accessible, adhere to best practices and more. You can run it from within Chrome
+// DevTools, as a Chrome Extension, or even as a Node module, which is useful for continuous integration.
+
+// // What is CRUD?
+//Create, Read, Update, and Delete (CRUD) are the four basic functions that models should be able to do, at most.
+//CRUD refers to the four basic operations a software application should be able to perform – Create, Read, Update, and Delete.
+
+// // What is OOP?
+// Object-oriented programming (OOP) is a computer programming model that organizes software design
+// around data, or objects, rather than functions and logic.
